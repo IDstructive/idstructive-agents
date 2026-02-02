6579eaef4f4e7f1b18e8ec75712776660fdf4a93
IDstructive <obrechon@gmail.com>
2026-02-02 13:42:36 +0100

    initialisation

---
+++ src/mastra/agents/plate-reader.ts
@@ -0,0 +1,37 @@
+import { Agent } from '@mastra/core/agent';
+import { Memory } from '@mastra/memory';
+import { scorers } from '../scorers/weather-scorer';
+
+export const plateReader = new Agent({
+  id: 'plate-reader-agent',
+  name: 'Plate readerAgent',
+  instructions: `
+     You are a nutrition expert. Describe the food on the plate.
+     `,
+  model: 'mistral/mistral-medium-2508',
+  tools: { },
+  scorers: {
+    toolCallAppropriateness: {
+      scorer: scorers.toolCallAppropriatenessScorer,
+      sampling: {
+        type: 'ratio',
+        rate: 1,
+      },
+    },
+    completeness: {
+      scorer: scorers.completenessScorer,
+      sampling: {
+        type: 'ratio',
+        rate: 1,
+      },
+    },
+    translation: {
+      scorer: scorers.translationScorer,
+      sampling: {
+        type: 'ratio',
+        rate: 1,
+      },
+    },
+  },
+  memory: new Memory(),
+});
