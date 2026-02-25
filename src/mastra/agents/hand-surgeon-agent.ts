c852b26396715ee20072fde317db17b92441afa8
IDstructive <obrechon@gmail.com>
2026-02-25 14:48:42 +0100

    cleaning names

--- src/mastra/agents/hand-surgeon-agent.ts
+++ src/mastra/agents/hand-surgeon-agent.ts
@@ -2,9 +2,9 @@ import { Agent } from '@mastra/core/agent';
 import { Memory } from '@mastra/memory';
 import { scorers } from '../scorers/weather-scorer';
 
-export const plateReader = new Agent({
-  id: 'plate-reader-agent',
-  name: 'Plate reader Agent',
+export const handSurgeonAgent = new Agent({
+  id: 'hand-surgeon-agent',
+  name: 'Hand surgeon Agent',
   instructions: 'Tu es un médecien et un chirurgien spécialiste de la main. Tu es aussi un médecin urgentiste. Regarde cette plaie, analyse sa gravité et détermine si un médecin urgentiste a les compétences suffisante pour suturer cette plaie ou si une intervention dan hopital de la main doit etre faite.'
      ,
   // instructions: `
